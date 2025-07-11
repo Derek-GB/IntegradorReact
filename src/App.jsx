bc205651c8e3f21192efec6f13041204f378eec9
CarlosMorales <cfmorales191@gmail.com>
2025-07-10 21:58:21 -0600

    cambios en css formularioFusionado.css
    Se cambio las propiedades de .container, el width 100%, marginLeft 65 px, elimine el sidebar comentado ya que esta duplicado y no se utiliza

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './styles/formularioFusionado.css'; // Asegúrate de que la ruta sea correcta
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Sidebar from './components/Sidebar.jsx';
+import Sidebar from './components/sidebar.jsx';
 import Login from './components/Login.jsx';
 import Inicio from './components/Inicio.jsx';
 import PreFormulario from './components/PreFormulario.jsx';
-import RegistroSuministros from './components/RegistroSuministros.jsx';
+import RegistroSuministros from './components/registroSuministros.jsx';
 import AsignacionRecursos from './components/AsignacionRecursos.jsx';
 import BusquedaAlbergue from './components/BusquedaAlbergue.jsx';
 import AyudaForm from './components/AyudaForm.jsx';
@@ -15,7 +16,7 @@ import ListaProducto from './components/ListaProducto.jsx';
 import ListaAlbergue from './components/ListaAlbergue.jsx';
 import FormularioAbastecimiento from './components/FormularioAbarrotes.jsx';
 
-import './styles/formularioFusionado.css';
+
 
 const App = () => {
   const location = useLocation();
@@ -24,22 +25,24 @@ const App = () => {
   return (
     <>
       <div className="app-container">
-        {!isLogin && <Sidebar />}
+
+        {!isLogin && <Sidebar/>}
+
         <div className="container main-content">
           <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/inicio.jsx" element={<Inicio />} />
-            <Route path="/preFormulario.jsx" element={<PreFormulario />} />
-            <Route path="/registroSuministros.jsx" element={<RegistroSuministros />} />
-            <Route path="/asignacionRecursos.jsx" element={<AsignacionRecursos />} />
-            <Route path="/busquedaAlbergue.jsx" element={<BusquedaAlbergue />} />
-            <Route path="/ayudaForm.jsx" element={<AyudaForm />} />
-            <Route path="/registrarProducto.jsx" element={<RegistrarProducto />} />
-            <Route path="/registroAlbergue.jsx" element={<RegistroAlbergue />} />
-            <Route path="/registroUsuario.jsx" element={<RegistroUsuario />} />
-            <Route path="/listaProducto.jsx" element={<ListaProducto />} />
-            <Route path="/listaAlbergue.jsx" element={<ListaAlbergue />} />
-            <Route path="/formularioAbarrotes.jsx" element={<FormularioAbastecimiento />} />
+            <Route path="/Inicio.jsx" element={<Inicio />} />
+            <Route path="/PreFormulario.jsx" element={<PreFormulario />} />
+            <Route path="/RegistroSuministros.jsx" element={<RegistroSuministros />} />
+            <Route path="/AsignacionRecursos.jsx" element={<AsignacionRecursos />} />
+            <Route path="/BusquedaAlbergue.jsx" element={<BusquedaAlbergue />} />
+            <Route path="/AyudaForm.jsx" element={<AyudaForm />} />
+            <Route path="/RegistrarProducto.jsx" element={<RegistrarProducto />} />
+            <Route path="/RegistroAlbergue.jsx" element={<RegistroAlbergue />} />
+            <Route path="/RegistroUsuario.jsx" element={<RegistroUsuario />} />
+            <Route path="/ListaProducto.jsx" element={<ListaProducto />} />
+            <Route path="/ListaAlbergue.jsx" element={<ListaAlbergue />} />
+            <Route path="/FormularioAbarrotes.jsx" element={<FormularioAbastecimiento />} />
           </Routes>
 
         </div>
