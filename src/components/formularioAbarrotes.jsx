1e433776ae438df585fcb655b3df989cd3389100
Fernando Jose Bolivar Brenes <febolivarbr@est.utn.ac.cr>
2025-07-17 19:32:58 -0600

    Coreccion css

--- src/components/formularioAbarrotes.jsx
+++ src/components/formularioAbarrotes.jsx
@@ -5,23 +5,27 @@ import Verduras from './Verduras';
 import Olores from './Olores';
 import Abarrotes from './Abarrotes';
 import Limpieza from './Limpieza';
-import { AbastecimientoProvider } from '../context/contextoAbastecimiento';
-import { BrowserRouter } from 'react-router-dom'; 
-import '../styles/formularioFusionado.css'; // Asegúrate de tener este archivo con tus estilos
-
+import '../styles/formAbasteci.css';
 
 const FormularioAbastecimiento = () => {
   return (
-<div className="container main-content">
-  <h2>Formulario de Abastecimiento</h2>
-  {/* Secciones independientes */}
-  <Carnes />
-  <Proteinas />
-  <Verduras />
-  <Olores />
-  <Abarrotes />
-  <Limpieza />
-</div>
+    <div className="content-area">
+      <div className="top-header">
+        <h1>Formulario de Abastecimiento</h1>
+      </div>
+
+      <div className="main-content">
+        <div id="formularioAbarrotes">
+          {/* Secciones independientes */}
+          <Carnes />
+          <Proteinas />
+          <Verduras />
+          <Olores />
+          <Abarrotes />
+          <Limpieza />
+        </div>
+      </div>
+    </div>
   );
 };
 
